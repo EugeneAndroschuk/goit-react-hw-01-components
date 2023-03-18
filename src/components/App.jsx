56b057b5477225e78ebf4e4de2706bfab929012b
EugeneAndroschuk <enet2611@gmail.com>
2023-03-19 01:17:16 +0200

    next attemp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,12 @@ import data from '../data/data.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
 // import Profile from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
+// import Statistics from './Statistics/Statistics';
+// import Profile from './Profile/Profile';
+// import FriendList from './FriendList/FriendList';
+// import TransactionHistory from './TransactionHistory/TransactionHistory';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 
